c4a0101f0e21bcb282f2b893334b5229f46f8225
Mushfiq80 <aurpon80@gmail.com>
2025-03-01 16:50:17 +0600

    bookcard updated fixed h and w-full

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -11,12 +11,12 @@ interface Book {
 const BookCard = ({ book }: { book: Book }) => {
     const { writerName, bookTitle, ratings, bookCover } = book;
     return (
-        <div className="w-[220px] mt-10 border border-gray-100 shadow-lg bg-slate-100 p-2 flex flex-col justify-between h-full">
+        <div className="w-full h-[400px] mt-10 border border-gray-100 shadow-lg bg-slate-100 p-2 flex flex-col justify-between">
             <div className="flex flex-col items-center space-y-2">
                 <img src={bookCover} alt="book1" className="w-40 h-60 object-cover" />
                 <Rating key={ratings}></Rating>
-                <h2 className="text-xl text-center">{bookTitle}</h2>
-                <p className="text-gray-600">{writerName}</p>
+                <h2 className="text-center">{bookTitle}</h2>
+                <p className="text-gray-600">{writerName? writerName:"Author Unlisted"}</p>
             </div>
             <button className="btn btn-success w-10/12 btn-sm mt-2 self-center">Add to My List</button>
         </div>
